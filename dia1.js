4595cf8b3e9da4989dee6266af1deed3c0394871
jefryne <jefryne89@gmail.com>
2023-09-26 09:39:50 -0500

    traductor

---
+++ dia1.js
@@ -0,0 +1,24 @@
+idioma_detectado = ""
+
+function traducir(texto_traducir, idioma_detectado) {
+    
+    fetch("https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to="+((idioma_detectado=="") ? "en": idioma_detectado)+"&to=fr&to=zh",{
+        method: "POST",
+        headers: {
+            "Ocp-Apim-Subscription-Key": "c57f563494ad41df92dfbe31871ad5cc",
+            "Ocp-Apim-Subscription-Region": "eastus",
+            "Content-Type": "application/json"
+            
+        },
+        body: JSON.stringify([{'text': texto_traducir}]) 
+    })
+    .then(respuesta => respuesta.json())
+    .then(data =>{
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
+
+traducir("pato",idioma_detectado)
